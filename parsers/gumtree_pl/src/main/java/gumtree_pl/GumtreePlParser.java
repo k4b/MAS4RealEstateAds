e456b0b46d8998a81517ad7fa4f6adef1f771b2d
Mariusz Kopacz <mariusz.kopacz90@gmail.com>
2014-01-14 22:53:55 +0100

    add basic version of gumtree parser

--- parsers/gumtree_pl/src/main/java/gumtree_pl/GumtreePlParser.java
+++ parsers/gumtree_pl/src/main/java/gumtree_pl/GumtreePlParser.java
@@ -1,39 +1,186 @@
 package gumtree_pl;
+
 import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import common.ads.Ad;
 import common.parsers.ParserAgent;
 
-
 public class GumtreePlParser extends ParserAgent {
 
-  public GumtreePlParser(int maxPages) {
-    super(maxPages);
-    // TODO Auto-generated constructor stub
-  }
+	private static final String TITLE_ID = "preview-local-title";
+	private static final String DESCRIPTION_ID = "preview-local-desc";
+	private static final String ADDRESS_KEY = "Adres";
+	private static final String PRICE_KEY = "Cena";
+	private static final String NUM_BEDROOMS_KEY = "Liczba pokoi";
+	private static final String NUM_BATHROOMS_KEY = "Liczba ³azienek";
+	private static final String AREA_KEY = "Wielkoœæ (m2)";
+	private static final String LAST_UPDATE_KEY = "Ostatnio zmieniony";
+	private static final String CREATION_DATE_KEY = "Data dodania";
+	
+	public GumtreePlParser(int maxPages) {
+		super(maxPages);
+		// TODO Auto-generated constructor stub
+	}
 
-  @Override
-  public URL constructRequestUrl() {
-    // TODO Auto-generated method stub
-    return null;
-  }
+	@Override
+	public URL constructRequestUrl() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-  @Override
-  public void startParsing(URL url, int maxPages) {
-    // TODO Auto-generated method stub
-    
-  }
+	@Override
+	public void startParsing(URL url, int maxPages) {
+		// TODO Auto-generated method stub
+	}
 
-  @Override
-  public void parseSearchResults(URL url) {
-    // TODO Auto-generated method stub
-    
-  }
+	@Override
+	public void parseSearchResults(URL url) {
+		Document doc = downloadWebpage(url);
+		Ad ad = new Ad();
+		
+		String title = doc.getElementById(TITLE_ID).text().trim();
+		ad.setTitle(title);
+		
+		String description = doc.getElementById(DESCRIPTION_ID).text().trim();
+		ad.setDescription(description);
 
-  @Override
-  public void parseDetails(URL url, Ad a) {
-    // TODO Auto-generated method stub
-    
-  }
-  
+		Element table = doc.getElementById("attributeTable");
+		Elements rows = table.getElementsByTag("tr");
+
+		String key, value;
+		for (Element row : rows) {
+			Elements columns = row.children();
+			key = columns.get(0).text().trim();
+			value = columns.get(1).text().trim();
+
+			try {
+				switch (key) {
+					case ADDRESS_KEY:
+						Map<String, String> parts = getAddress(value);
+						ad.setCity(parts.get("city"));
+						ad.setStreet(parts.get("street"));
+						break;
+					case PRICE_KEY:
+						int price = getPrice(value);
+						ad.setPrice(price);
+						break;
+					case NUM_BEDROOMS_KEY:
+						int numBedrooms = getNumberOfBedrooms(value);
+						ad.setNumBedrooms(numBedrooms);
+						break;
+					case NUM_BATHROOMS_KEY:
+						int numBathrooms = getNumberOfBathrooms(value);
+						ad.setNumBathrooms(numBathrooms);
+						break;
+					case AREA_KEY:
+						double area = getArea(value);
+						ad.setArea(area);
+						break;
+					case LAST_UPDATE_KEY:
+						Date lastUpdate = getLastUpdate(value);
+						ad.setLastUpdate(lastUpdate);
+						break;
+					case CREATION_DATE_KEY:
+						Date creationDate = getCreationDate(value);
+						ad.setCreationDate(creationDate);
+						break;
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+				continue;
+			}
+		}
+
+		int pricePerMeter = calculatePricePerMeter(ad.getPrice(), ad.getArea());
+		ad.setPricePerMeter(pricePerMeter);
+		
+		// int floor; BD
+		// int constructionYear; BD
+		// int numFloors; BD
+		
+		String link = url.toString();
+		ad.setLink(link);
+		
+		System.out.println(ad.toString());
+	}
+
+	@Override
+	public void parseDetails(URL url, Ad a) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private Map<String, String> getAddress(String value) {
+		String withoutShowMap = value.replace("Poka¿ mapê", "");
+		String withoutCountry = withoutShowMap.replace("Polska", "");
+		String withoutCommas = withoutCountry.replaceAll(",", "").trim();
+		String[] parts = withoutCommas.split(" ");
+		
+		String city = null, street = null;
+		Map<String, String> result = new HashMap<String, String>();
+		if(parts.length == 1) {
+			city = parts[0];
+		} else if (parts.length == 2) {
+			city = parts[1];
+			street = parts[0];
+		}
+		// String district; BD
+		
+		result.put("city", city);
+		result.put("street", street);
+		return result;
+	}
+
+	private int getPrice(String value) {
+		String onlyNumbers = value.replaceAll("[^0-9]", "");
+		String withoutDecimals = onlyNumbers.substring(0, onlyNumbers.length() - 2);
+		int price = Integer.parseInt(withoutDecimals);
+		return price;
+	}
+
+	private int getNumberOfBedrooms(String value) {
+		if(value.equals("Kawalerka lub garsoniera")) {
+			return 1;
+		}
+		String onlyNumbers = value.replaceAll("[^0-9]", "");
+		int numBedrooms = Integer.parseInt(onlyNumbers);
+		return numBedrooms;
+	}
+	
+	private int getNumberOfBathrooms(String value) {
+		String onlyNumbers = value.replaceAll("[^0-9]", "");
+		int numBathrooms = Integer.parseInt(onlyNumbers);
+		return numBathrooms;
+	}
+
+	private double getArea(String value) {
+		double area = Double.parseDouble(value);
+		return area;
+	}
+	
+	private Date getLastUpdate(String value) throws ParseException {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		Date lastUpdate = dateFormat.parse(value);
+		return lastUpdate;
+	}
+
+	private Date getCreationDate(String value) throws ParseException {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		Date creationDate = dateFormat.parse(value);
+		return creationDate;
+	}
+	
+	private int calculatePricePerMeter(int price, double area) {
+		int pricePerMeter = (int) (price / area);
+		return pricePerMeter;
+	}
 }
