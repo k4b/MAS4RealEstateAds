59a4a5beb68c91f7701db2a7e3e0a23f21cb16b1
Przemek W <wierucki.p@gmail.com>
2014-01-17 02:03:20 +0100

    Main for szybko.pl

---
+++ parsers/szybko_pl/src/main/java/szybko_pl/Main.java
@@ -0,0 +1,26 @@
+package szybko_pl;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Gelu on 15.01.14.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        SzybkoFilterBuilder szybkoFilterBuilder = new SzybkoFilterBuilder();
+        String builded = szybkoFilterBuilder.withCity("Warszawa").withPriceMax(500000).build();
+        System.out.println(builded);
+        SzybkoPlParser parser = new SzybkoPlParser(30);
+        URL url = null;
+        try {
+             url = new URL (builded);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        parser.startParsing(url,30);
+
+
+    }
+}
